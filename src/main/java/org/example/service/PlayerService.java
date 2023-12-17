17379b9016b289b3e232e229e4c0c4fc067b0f98
Hilleger <mguliev2003@gmail.com>
2023-12-18 00:44:06 +0300

    updated PlayerService, added getPlayersInfo

--- src/main/java/org/example/service/PlayerService.java
+++ src/main/java/org/example/service/PlayerService.java
@@ -1,9 +1,39 @@
 package org.example.service;
 
+import org.example.entity.EstablishmentCard;
+import org.example.entity.LandmarkCard;
+import org.example.entity.Player;
+import org.example.repository.PlayerRepository;
+import org.example.to.domain.game.PlayerTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerService {
 
     public static void getPlayersInfo() {
+        List<Player> players = PlayerRepository.getPlayers();
+        for (Player player: players){
+            System.out.println("NAME: " + player.getClient().getName());
+            System.out.println("COINS: " + player.getCoins());
+            for (EstablishmentCard card: player.getEstablishmentCards()){
+                System.out.println("CARD NAME: " + card.getTitle());
+                System.out.println("DESCRIPTION: " + card.getDescription());
+                System.out.print("COST: " + card.getCost() + "  ");
+                System.out.print("TYPE: " + card.getType() + "  ");
+                System.out.print("EFFECT: " + card.getPassive() + "  ");
+                System.out.print("COUNT: " + card.getCount() + "  ");
+                System.out.println("CONDITION: " + card.getCondition());
+            }
 
+            for (LandmarkCard card: player.getLandmarkCards()){
+                System.out.println("CARD NAME: " + card.getTitle());
+                System.out.println("DESCRIPTION: " + card.getDescription());
+                System.out.print("COST: " + card.getCost() + "  ");
+                System.out.println("STATUS " + card.isStatus() + "\n");
+
+            }
+        }
     }
 
     public static void getPlayersInfoExceptClient(String clientName){
