6cd251a81d53f5bda231b02e1f76a1331536fcd6
Maksim Navoichyk <makseke@mail.ru>
2023-12-18 21:53:37 +0300

    launch update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import org.example.controller.LaunchController;
 import org.example.repository.ClientsRepository;
 
-import org.example.repository.PlayerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,14 +20,14 @@ public class App {
     public static final Logger logger = LoggerFactory.getLogger("MACHICORO");
 
     public static void main(String[] args) {
-        System.out.println("                               __                            ____                             \n" +
-                            " /'\\_/`\\                      /\\ \\         __               /\\  _`\\                           \n" +
-                            "/\\      \\      __       ___   \\ \\ \\___    /\\_\\              \\ \\ \\/\\_\\    ___    _ __    ___   \n" +
-                            "\\ \\ \\__\\ \\   /'__`\\    /'___\\  \\ \\  _ `\\  \\/\\ \\              \\ \\ \\/_/_  / __`\\ /\\`'__\\ / __`\\ \n" +
-                            " \\ \\ \\_/\\ \\ /\\ \\L\\.\\_ /\\ \\__/   \\ \\ \\ \\ \\  \\ \\ \\              \\ \\ \\L\\ \\/\\ \\L\\ \\\\ \\ \\/ /\\ \\L\\ \\\n" +
-                            "  \\ \\_\\\\ \\_\\\\ \\__/.\\_\\\\ \\____\\   \\ \\_\\ \\_\\  \\ \\_\\              \\ \\____/\\ \\____/ \\ \\_\\ \\ \\____/\n" +
-                            "   \\/_/ \\/_/ \\/__/\\/_/ \\/____/    \\/_/\\/_/   \\/_/               \\/___/  \\/___/   \\/_/  \\/___/ \n" +
-                            "                                                                                              ");
+        System.out.println("" +
+                "███╗   ███╗ █████╗  ██████╗██╗  ██╗██╗     ██████╗ ██████╗ ██████╗  ██████╗ \n" +
+                "████╗ ████║██╔══██╗██╔════╝██║  ██║██║    ██╔════╝██╔═══██╗██╔══██╗██╔═══██╗\n" +
+                "██╔████╔██║███████║██║     ███████║██║    ██║     ██║   ██║██████╔╝██║   ██║\n" +
+                "██║╚██╔╝██║██╔══██║██║     ██╔══██║██║    ██║     ██║   ██║██╔══██╗██║   ██║\n" +
+                "██║ ╚═╝ ██║██║  ██║╚██████╗██║  ██║██║    ╚██████╗╚██████╔╝██║  ██║╚██████╔╝\n" +
+                "╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝     ╚═════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ \n" +
+                "                                                                            ");
         logger.info("VERSION 0.1 HAS STARTED");
         LaunchController launchController = new LaunchController();
         launchController.launchController();
